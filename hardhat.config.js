a3afaf6efb0477f697b3a2e9d55bed628b7c9466
원민관 <mkwan5741@gmail.com>
2025-12-01 16:48:11 +0900

    chore: Hardhat 기본 설정과 env 테스트

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,28 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const DEFAULT_COMPILER_SETTINGS = {
+  version: "0.8.28",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    },
+  },
+};
+
 module.exports = {
-  solidity: "0.8.28",
+  solidity: DEFAULT_COMPILER_SETTINGS,
+
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+
+    monad_testnet: {
+      url: process.env.MONAD_RPC_URL || "",
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 80000,
+    },
+  },
 };
